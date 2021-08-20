f3808d47dd8e406aae02cd839de7a408bc01edb6
William Daghouz <w.daghouz@gmail.com>
2021-08-20 12:20:10 +0200

    Added images to masthead component.

--- src/components/indexPage/masthead/index.tsx
+++ src/components/indexPage/masthead/index.tsx
@@ -1,7 +1,27 @@
 import React, { FC } from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Masthead: FC = () => {
-  return <section></section>
+  return (
+    <section>
+      <StaticImage
+        src='../../../images/hero-img-landscape.jpeg'
+        alt='Light bulb standing on the floor, turned on and connected to a wall socket.'
+        loading='eager'
+        formats={['auto', 'webp', 'avif']}
+        placeholder='blurred'
+        layout='fullWidth'
+      />
+      <StaticImage
+        src='../../../images/hero-img-portrait.jpeg'
+        alt='Light bulb standing on the floor, turned on and connected to a wall socket.'
+        loading='eager'
+        formats={['auto', 'webp', 'avif']}
+        placeholder='blurred'
+        layout='fullWidth'
+      />
+    </section>
+  )
 }
 
 export default Masthead

c4058a16b8f605c2f6e02583bb5137fc8ef9a491
William Daghouz <w.daghouz@gmail.com>
2022-03-25 20:52:04 +0100

    Loaded one font-families localy instead of from package.

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,5 +1,7 @@
 import './src/styles/fonts/montserrat900.css'
 import './src/styles/fonts/montserrat800.css'
 import './src/styles/fonts/montserrat700.css'
+import './src/styles/fonts/raleway600.css'
+import './src/styles/fonts/raleway500.css'
+import './src/styles/fonts/raleway400.css'
 import '@fontsource/nothing-you-could-do'
-import '@fontsource/raleway'
